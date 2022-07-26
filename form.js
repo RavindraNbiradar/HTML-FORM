409a33afd24ccd314bd797cfa53f32f2b802d118
shivpatil005 <shiv.patil05@gmail.com>
2022-07-26 13:31:29 +0530

    form

---
+++ form.js
@@ -0,0 +1,13 @@
+function onsubmit() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var date = document.getElementById("date");
+    var time = document.getElementById("time");
+    console.log(name.value);
+    console.log(email.value);
+    console.log(phone.value);
+    console.log(date.value);
+    console.log(time.value);
+}
+
